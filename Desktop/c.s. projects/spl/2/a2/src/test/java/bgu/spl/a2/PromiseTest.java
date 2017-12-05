d9d6f19e08cb9cd4140b4ee93827a297e3f3cc71
adamshindel <adamsh@psot.bgu.ac.il>
2017-12-05 20:35:35 +0200

    merged

--- Desktop/c.s. projects/spl/2/a2/src/test/java/bgu/spl/a2/PromiseTest.java
+++ Desktop/c.s. projects/spl/2/a2/src/test/java/bgu/spl/a2/PromiseTest.java
@@ -1,129 +1,113 @@
 package bgu.spl.a2;
 
 import static org.junit.Assert.*;
-
-import java.util.concurrent.CountDownLatch;
-
+//import java.util.concurrent.CountDownLatch;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import bgu.spl.a2.Promise;
 
 public class PromiseTest {
-	
-	Promise<Integer> x;
-
+	/**
+	 * we are going to test all the methods of Promise
+	 */
 	@Before
 	public void setUp() throws Exception {
-		x=new Promise<>();
 	}
-
 	@After
 	public void tearDown() throws Exception {
 	}
-
-	
+	/**
+	 * checking if method get of Promise returns a proper value<br>
+	 * (get is atomic method)
+	 */
 	@Test
 	public void testGet() {
-		try{
-			Thread t = new Thread(new Runnable() {
-				
-				@Override
-				public void run() {
-					//x.resolve(5);
-					try {
-						x.resolve(5);
-					}
-					catch (IllegalStateException e) {
-						// TODO Auto-generated catch block
-						fail("Get is not working");
-					}
-					catch(Exception e){
-						fail("Get is not working");
-					}
-				}				
-			});
-			
-			t.start();
-			t.join();
-			assertEquals(5, (int)x.get());
+		Promise<Integer> x = new Promise<>();
+		try {
+			try {
+				x.get();
+				fail();
+			}catch(Exception e) {}
+			x.resolve(5);
+			Integer value =new Integer( x.get());
+			assertTrue(value != null && value==5);
 		}
-		catch (Exception e) {
-			fail("Get Method Failed");
+		catch(Exception e){
+			fail();
 		}
-		
 	}
-
+	/**
+	 * checking if method isResolved of Promise returns a proper value<br>
+	 * (isResolved is atomic method)
+	 */
 	@Test
 	public void testIsResolved() {
-		try{
-			Thread t = new Thread(new Runnable() {
-				
-				@Override
-				public void run() {
-					//x.resolve(5);
-					try {
-						x.resolve(5);
-					}
-					catch (IllegalStateException e) {
-						// TODO Auto-generated catch block
-						fail("IsResolved is not working");
-					}
-					catch(Exception e){
-						fail("IsResolved is not working");
-					}
-				}				
-			});
-			
-			t.start();
-			t.join();
+		Promise<Integer> x = new Promise<>();
+		try {
+			if(x.isResolved())
+				fail();
+			x.resolve(5);
 			assertTrue(x.isResolved());
 		}
-		catch (Exception e) {
-			fail("IsResolved Method Failed");
+		catch(Exception e){
+			fail();
 		}
 	}
-
+	/**
+	 * checking if method Resolve of Promise does the flowing things properly:<br>
+	 * 1.sets the result value to the new value<br>
+	 * 2.triggers all the subscribed call backs
+	 */
 	@Test
 	public void testResolve() throws InterruptedException {
-		try{
-			Thread t = new Thread(new Runnable() {			
-				@Override
-				public void run() {
-					//x.resolve(5);
-					try {
-						x.resolve(5);
-						try{
-							x.resolve(6);
-							fail("Resolve is not working");
-						}
-						catch(IllegalStateException e){
-							int value = x.get();
-							assertEquals(5, value);
-						}
-						catch(Exception e){
-							fail();
-						}
-					}
-					catch(Exception e){
-						fail("IsResolved is not working");
-					}
-				}				
-			});
-			t.start();
-			t.join();
+		Promise<Integer> x = new Promise<>();
+		final boolean[] passed = {false};
+		try {
+			x.subscribe(new callback() {
+		        @Override
+				public void call() {
+					passed[0]=true;
+				}
+		    });
+			x.resolve(5);
+			try{
+				x.resolve(6);
+				fail();
+			}
+			catch(IllegalStateException e){
+				Integer value =new Integer( x.get());
+				assertTrue(value != null && value==5 && passed[0]==true);
+			}
+			catch(Exception e){
+				fail();
+			}
 		}
-		catch (Exception e) {
-			fail("IsResolved Method Failed");
-		}		
+		catch(Exception e){
+			fail();
+		}
 	}
-	
-	
-
+	/**
+	 *checking if method subscribe of Promise adds a callback to be called when the object is resolved<br>
+	 *(isResolved is atomic method)
+	 */
 	@Test
 	public void testSubscribe() {
-		fail("Not yet implemented");
+		Promise<Integer> x = new Promise<>();
+		final boolean[] passed = {false};
+		try {
+			x.subscribe(new callback() {
+				@Override
+				public void call() {
+					// TODO Auto-generated method stub
+					passed[0] = true;
+				}
+			});
+			x.resolve(5);
+			assertTrue(passed[0]);
+		}
+		catch(Exception e){
+			fail();
+		}
 	}
-
 }
--- Desktop/c.s. projects/spl/2/a2/src/test/java/bgu/spl/a2/VersionMonitorTest.java
+++ Desktop/c.s. projects/spl/2/a2/src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -1,34 +1,114 @@
 package bgu.spl.a2;
 
 import static org.junit.Assert.*;
-
+//import java.time.chrono.ThaiBuddhistEra;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import bgu.spl.a2.VersionMonitor;
 
 public class VersionMonitorTest {
 
+	//the object under test
+	VersionMonitor out;
+	
+	/**
+	 * <h1> setup method</h1>
+	 * <p> creating a new monitor for each test </p>
+	 * @return a new monitor 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
+		try {
+			out = new VersionMonitor();
+		}catch (Exception e) {
+			fail("creation of the vertion monitor failed");
+		}
+			
 	}
+	
 
+	/**
+	 * <h1> tearDoen method</h1>
+	 * <p> Destroy the monitor under test-for garbage collection</p> 
+	 * @throws Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
+		try {
+			out=null;
+		}catch(Exception e) {
+			
+		}
 	}
 
+	/**
+	 * <h1>testGetVersion</h1>
+	 * <p> test whether the version number has been retrieved </p>
+	 * @Pre the monitor version is 0 
+	 * @Inv the monitor version should stay 0
+	 * @throws Exception
+	 */
 	@Test
 	public void testGetVersion() {
-		fail("Not yet implemented");
+		try {
+			int v=out.getVersion();
+			assertEquals(0, v);
+		}
+		catch(Exception e){
+			fail("geting vertion failed");
+		}
 	}
 
+	/**
+	 * <h1>TestInc</h1>
+	 * <p> testing that the version number indeed changes when using {@link Inc()}</p>
+	 * @param out.version is the current monitor version 
+	 * @post monitor version should be version+1
+	 * @throws Exception
+	 */
 	@Test
 	public void testInc() {
-		fail("Not yet implemented");
+		try {
+			int v = out.getVersion();
+			out.inc();
+			assertNotEquals(v, out.getVersion());
+			assertEquals(v+1, out.getVersion());
+			
+		}catch(Exception e) {
+			fail("inc does not change the vertion number");
+		}
 	}
-
+	
+	
+	/**
+	 * <h1>Test await</h1>
+	 * <p>testing the ability of the {@link Await()} method to freeze a thread <br>,
+	 * the test will create a thread that will run {@link Await()}, we expect <br>
+	 * the thread to get into waiting state-we will check that. after words we <br>
+	 * will use {@link Inc()}, we expect that the thread will get out of waiting mode<br>
+	 * -and we will check that too </p>
+	 * @Pre t1 should be at running state
+	 * @Inv t1 should get out of running state <=> {@link Await()} is called
+	 * @throws InterruptedException
+	 */
 	@Test
-	public void testAwait() {
-		fail("Not yet implemented");
+	public void testAwait() throws InterruptedException {
+		int version = out.getVersion();
+		Thread t= new Thread(()->{
+			try {
+				out.await(version);
+			} catch (InterruptedException e) {
+				fail();
+			}
+		});
+		t.start();
+		t.join();
+		if(t.getState()!=Thread.State.WAITING)
+			fail();
+		out.inc(); //should bring t out of waiting state
+		Thread.sleep(2000);//giving time to t to go out of waiting state
+		assertNotEquals(t.getState(), Thread.State.WAITING);
 	}
-
 }
